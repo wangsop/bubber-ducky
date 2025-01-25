346fdfefad87f846641c23eeccb8591cf71b5c41
wangsop <sophia.riverfront@gmail.com>
2025-01-24 21:15:00 -0500

    Obstacle update
    Added obstacles/functionality for them (now can be hit, will disappear after being hit, will cause player to lose a bubble) - left a temp comment in place of Slow() function for when it gets added to the game

--- Assets/Sophia/ObstacleScript.cs
+++ Assets/Sophia/ObstacleScript.cs
@@ -2,14 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BubbleHitScript : MonoBehaviour
+public class ObstacleScript : MonoBehaviour
 {
     [SerializeField] BubbleCountScript bcs;
 
     // Update is called once per frame
     private void OnTriggerEnter(Collider other)
     {
-        bcs.addBubble();
+        bcs.loseBubble();
         Destroy(gameObject);
+        //PlayerController.Slow();
     }
 }
