a26e010199be55387ad0c269580a248813097e64
hannahrajan <66148049+hannahrajan@users.noreply.github.com>
2025-01-26 12:38:42 -0500

    Scaling + Restart Bug Fixes
     - Scaling fixed for levels
     - Restart now spawns properly :)

--- Assets/Rachel/Scripts/BubberController.cs
+++ Assets/Rachel/Scripts/BubberController.cs
@@ -51,6 +51,7 @@ public class BubberController : MonoBehaviour
 
     private void Update()
     {
+        //Debug.Log(state);
         if (state == BubberState.PLAY)
         {
             CheckInput();
